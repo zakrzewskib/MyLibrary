c31c4d3ce61c698d5d08d7e6c23fd07b8964ae1e
Bartosz Zakrzewski <barzak200@gmail.com>
2022-01-05 13:41:05 +0100

    @foreach (var author in item.Authors) works

--- MyLibrary.WebApp/Models/BookVM.cs
+++ MyLibrary.WebApp/Models/BookVM.cs
@@ -11,9 +11,6 @@ namespace MyLibrary.WebApp.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string ImageURL { get; set; }
-        public ICollection<Author> Authors { get; set; }
-        public ICollection<Comment> Comments { get; set; }
-        public PublishingHouse PublishingHouse { get; set; }
-        public ICollection<Accessibility> Accessibilities { get; set; }
+        public List<Author> Authors { get; set; }
     }
 }
