9e9698502c48cb8edf23e2d493d3d1bbc2af51bf
Bartosz Zakrzewski <barzak200@gmail.com>
2022-01-08 16:53:46 +0100

    Creating book via react does not work

---
+++ MyLibrary.Client/apps/books/pages/book.page.create.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+// actions
+import { BookActions } from "../store/books.store";
+
+const BookCreatePage = ({ ...props }) => {
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
+};
+
+export default BookCreatePage;
--- MyLibrary.Client/apps/books/router.js
+++ MyLibrary.Client/apps/books/router.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import BooksPage from "./pages/books.page";
+import BookCreatePage from "./pages/book.page.create";
 
 export const Router = props => {
   return (
     <Switch>
-      <Route exact path={"/Book"} component={BooksPage} />
+      <Route exact path={"/"} component={BooksPage} />
+      <Route exact path={"/Book/Create/"} component={BookCreatePage} />
     </Switch>
   );
 };
